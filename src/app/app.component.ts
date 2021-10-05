63970cb3c4fc472c34aea7f0f42399d4af6387fe
Anqi Yu <acoolbanana23@gmail.com>
2021-10-05 00:41:07 -0500

    add onScroll()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, Subscription } from 'rxjs';
+import { fromEvent, Observable, Subscription } from 'rxjs';
+import { throttleTime } from 'rxjs/operators';
 import { Papa, ParseResult } from 'ngx-papaparse';
 
 @Component({
@@ -9,12 +16,21 @@ import { Papa, ParseResult } from 'ngx-papaparse';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  public rows: string[][] = [];
+  // @ts-ignore
+  @ViewChild('container', { static: true }) container: ElementRef;
 
+  public rows: string[][] = [];
+  public cellWidthInPx = 200;
+  public cellCount = 5;
+  public containerWidthInPx = this.cellWidthInPx * this.cellCount;
+
+  private filePath = '../assets/acho_export_virtual_scroll.csv';
+  private first50RowsWithAllColumns: string[][] = [];
+  private scrollStartIndex = 0;
+  private scrollEndIndex = this.cellCount * 3;
   private subscription: Subscription = new Subscription();
 
-  private readonly filePath = '../assets/acho_export_virtual_scroll.csv';
-  private readonly cellWidthInPx = 200;
+  public posMap: any = { 0: 0, min: 1, max: 15 };
 
   constructor(private http: HttpClient, private papa: Papa) {}
 
@@ -25,8 +41,14 @@ export class AppComponent implements OnInit, OnDestroy {
     );
   }
 
-  public ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+  public ngAfterViewInit() {
+    this.buttonClick();
+  }
+
+  public buttonClick() {
+    this.subscription = fromEvent(this.container.nativeElement, 'scroll')
+      .pipe(throttleTime(50))
+      .subscribe(() => this.onScroll());
   }
 
   private loadFile(filePath: string): Observable<string> {
@@ -36,22 +58,99 @@ export class AppComponent implements OnInit, OnDestroy {
   private parseData(data: string): void {
     this.papa.parse(data, {
       complete: (result: ParseResult) => {
-        const first50RowsWithAllColumns: string[][] = result.data.slice(0, 50);
-        const first50RowsWithFirst15Columns: string[][] =
-          first50RowsWithAllColumns.map((row) => row.slice(0, 15));
-        this.rows = first50RowsWithFirst15Columns;
+        this.first50RowsWithAllColumns = result.data.slice(0, 50);
+        this.updateRows();
       },
     });
+  }
+
+  private updateRows(): void {
+    const data = this.first50RowsWithAllColumns.map((row) =>
+      row.slice(this.scrollStartIndex, this.scrollEndIndex)
+    );
+    this.computeWidth(data);
+    console.log(this.posMap);
+    this.rows = data;
+  }
+
+  private computeWidth(data: string[][]) {
+    const max = Array(15).fill(0);
+    for (let i = 1; i < data.length; i++) {
+      for (let j = 0; j < data[0].length; j++) {
+        max[j] = Math.max(max[j], data[i][j] ? data[i][j].length : 0);
+      }
+    }
+    for (let i = 0; i < 15; i++) {
+      const index = +data[0][i].split('field_')[1];
+      if (this.posMap[index] !== undefined) {
+        continue;
+      }
+      if (this.posMap[index - 1] !== undefined) {
+        this.posMap[index] = Math.max(max[i] * 8, 200) + this.posMap[index - 1];
+      } else if (this.posMap[index + 1] !== undefined) {
+        this.posMap[index] =
+          -Math.max(max[i + 1] * 8, 200) + this.posMap[index + 1];
+      }
+    }
   }
 
   private handleLoadFileError(error: HttpErrorResponse): void {
     console.log(error.message);
   }
 
-  public getLeft(index: number): string {
+  public getLeft(index: number): number {
     const columnNames = this.rows[0];
-    const columnName = columnNames[index];
-    const columnIndex = parseInt(columnName.split('field_')[1]) - 1;
-    return this.cellWidthInPx * columnIndex + 'px';
+    const columnIndex =
+      index >= columnNames.length
+        ? parseInt(columnNames[index - 1].split('field_')[1])
+        : parseInt(columnNames[index].split('field_')[1]) - 1;
+    return this.posMap[columnIndex];
+  }
+
+  public onScroll() {
+    const scrollLeft = this.container.nativeElement.scrollLeft;
+    const leftBound = this.posMap[this.scrollStartIndex];
+    const rightBound = this.posMap[this.scrollEndIndex - 1] - 1200;
+
+    console.error('scrollLeft', scrollLeft);
+    console.error('leftBound', leftBound);
+    console.error('rightBound', rightBound);
+
+    if (scrollLeft > rightBound) {
+      this.incrementScrollIndex();
+    } else if (
+      scrollLeft < leftBound &&
+      this.scrollStartIndex >= this.cellCount
+    ) {
+      this.decrementScrollIndex();
+    }
+    for (
+      let i = Math.min(this.scrollStartIndex + 1, this.posMap.min);
+      i <= Math.max(this.scrollEndIndex, this.posMap.max);
+      i++
+    ) {
+      if (this.posMap[i] !== undefined && i < this.scrollStartIndex) {
+        delete this.posMap[i];
+      } else if (this.posMap[i] !== undefined && i > this.scrollEndIndex) {
+        delete this.posMap[i];
+      }
+    }
+    this.posMap.min = this.scrollStartIndex + 1;
+    this.posMap.max = this.scrollEndIndex;
+    this.updateRows();
+  }
+
+  private incrementScrollIndex(): void {
+    this.scrollStartIndex += this.cellCount;
+    this.scrollEndIndex += this.cellCount;
+  }
+
+  private decrementScrollIndex(): void {
+    this.scrollStartIndex -= this.cellCount;
+    this.scrollEndIndex -= this.cellCount;
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
