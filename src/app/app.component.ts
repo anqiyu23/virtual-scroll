f00e60488c9767fd2649e274a13f38926bdd04b3
Anqi Yu <acoolbanana23@gmail.com>
2021-10-05 16:58:43 -0500

    columnIndexToLeftMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,21 +16,22 @@ import { Papa, ParseResult } from 'ngx-papaparse';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  // @ts-ignore
-  @ViewChild('container', { static: true }) container: ElementRef;
+  @ViewChild('container') public container: ElementRef = {} as ElementRef;
 
   public rows: string[][] = [];
-  public cellWidthInPx = 200;
-  public cellCount = 5;
-  public containerWidthInPx = this.cellWidthInPx * this.cellCount;
+  public containerWidthInPx = 1000;
 
   private filePath = '../assets/acho_export_virtual_scroll.csv';
   private first50RowsWithAllColumns: string[][] = [];
   private scrollStartIndex = 0;
-  private scrollEndIndex = this.cellCount * 3;
+  private visibleColumnCount = 5;
+  private scrollEndIndex = this.visibleColumnCount * 3;
+  private columnIndexToLeftMap: any = {
+    0: 0,
+    min: this.scrollStartIndex + 1,
+    max: this.scrollEndIndex,
+  };
   private subscription: Subscription = new Subscription();
-
-  public posMap: any = { 0: 0, min: 1, max: 15 };
 
   constructor(private http: HttpClient, private papa: Papa) {}
 
@@ -39,16 +40,6 @@ export class AppComponent implements OnInit, OnDestroy {
       (data: string) => this.parseData(data),
       (error: HttpErrorResponse) => this.handleLoadFileError(error)
     );
-  }
-
-  public ngAfterViewInit() {
-    this.buttonClick();
-  }
-
-  public buttonClick() {
-    this.subscription = fromEvent(this.container.nativeElement, 'scroll')
-      .pipe(throttleTime(50))
-      .subscribe(() => this.onScroll());
   }
 
   private loadFile(filePath: string): Observable<string> {
@@ -68,28 +59,29 @@ export class AppComponent implements OnInit, OnDestroy {
     const data = this.first50RowsWithAllColumns.map((row) =>
       row.slice(this.scrollStartIndex, this.scrollEndIndex)
     );
-    this.computeWidth(data);
-    console.log(this.posMap);
+    this.setColumnIndexToLeftMap(data);
     this.rows = data;
   }
 
-  private computeWidth(data: string[][]) {
+  private setColumnIndexToLeftMap(rows: string[][]): void {
     const max = Array(15).fill(0);
-    for (let i = 1; i < data.length; i++) {
-      for (let j = 0; j < data[0].length; j++) {
-        max[j] = Math.max(max[j], data[i][j] ? data[i][j].length : 0);
+    for (let i = 1; i < rows.length; i++) {
+      for (let j = 0; j < rows[0].length; j++) {
+        max[j] = Math.max(max[j], rows[i][j] ? rows[i][j].length : 0);
       }
     }
+
     for (let i = 0; i < 15; i++) {
-      const index = +data[0][i].split('field_')[1];
-      if (this.posMap[index] !== undefined) {
+      const index = +rows[0][i].split('field_')[1];
+      if (this.columnIndexToLeftMap[index] !== undefined) {
         continue;
       }
-      if (this.posMap[index - 1] !== undefined) {
-        this.posMap[index] = Math.max(max[i] * 8, 200) + this.posMap[index - 1];
-      } else if (this.posMap[index + 1] !== undefined) {
-        this.posMap[index] =
-          -Math.max(max[i + 1] * 8, 200) + this.posMap[index + 1];
+      if (this.columnIndexToLeftMap[index - 1] !== undefined) {
+        this.columnIndexToLeftMap[index] =
+          Math.max(max[i] * 8, 200) + this.columnIndexToLeftMap[index - 1];
+      } else if (this.columnIndexToLeftMap[index + 1] !== undefined) {
+        this.columnIndexToLeftMap[index] =
+          -Math.max(max[i + 1] * 8, 200) + this.columnIndexToLeftMap[index + 1];
       }
     }
   }
@@ -98,56 +90,70 @@ export class AppComponent implements OnInit, OnDestroy {
     console.log(error.message);
   }
 
+  public ngAfterViewInit(): void {
+    this.subscription = fromEvent(this.container.nativeElement, 'scroll')
+      .pipe(throttleTime(50))
+      .subscribe(() => this.onScroll());
+  }
+
+  private onScroll(): void {
+    const scrollLeft = this.container.nativeElement.scrollLeft;
+    const leftBound = this.columnIndexToLeftMap[this.scrollStartIndex];
+    const rightBound =
+      this.columnIndexToLeftMap[this.scrollEndIndex] -
+      this.containerWidthInPx * 1.2;
+
+    if (scrollLeft > rightBound) {
+      this.incrementScrollIndex();
+    } else if (
+      scrollLeft < leftBound &&
+      this.scrollStartIndex >= this.visibleColumnCount
+    ) {
+      this.decrementScrollIndex();
+    }
+
+    for (
+      let i = Math.min(
+        this.scrollStartIndex + 1,
+        this.columnIndexToLeftMap.min
+      );
+      i <= Math.max(this.scrollEndIndex, this.columnIndexToLeftMap.max);
+      i++
+    ) {
+      if (
+        this.columnIndexToLeftMap[i] !== undefined &&
+        i < this.scrollStartIndex
+      ) {
+        delete this.columnIndexToLeftMap[i];
+      } else if (
+        this.columnIndexToLeftMap[i] !== undefined &&
+        i > this.scrollEndIndex
+      ) {
+        delete this.columnIndexToLeftMap[i];
+      }
+    }
+    this.columnIndexToLeftMap.min = this.scrollStartIndex + 1;
+    this.columnIndexToLeftMap.max = this.scrollEndIndex;
+    this.updateRows();
+  }
+
+  private incrementScrollIndex(): void {
+    this.scrollStartIndex += this.visibleColumnCount;
+    this.scrollEndIndex += this.visibleColumnCount;
+  }
+
+  private decrementScrollIndex(): void {
+    this.scrollStartIndex -= this.visibleColumnCount;
+    this.scrollEndIndex -= this.visibleColumnCount;
+  }
+
   public getLeft(index: number): number {
     const columnNames = this.rows[0];
     const columnIndex =
       index >= columnNames.length
         ? parseInt(columnNames[index - 1].split('field_')[1])
         : parseInt(columnNames[index].split('field_')[1]) - 1;
-    return this.posMap[columnIndex];
-  }
-
-  public onScroll() {
-    const scrollLeft = this.container.nativeElement.scrollLeft;
-    const leftBound = this.posMap[this.scrollStartIndex];
-    const rightBound = this.posMap[this.scrollEndIndex - 1] - 1200;
-
-    console.error('scrollLeft', scrollLeft);
-    console.error('leftBound', leftBound);
-    console.error('rightBound', rightBound);
-
-    if (scrollLeft > rightBound) {
-      this.incrementScrollIndex();
-    } else if (
-      scrollLeft < leftBound &&
-      this.scrollStartIndex >= this.cellCount
-    ) {
-      this.decrementScrollIndex();
-    }
-    for (
-      let i = Math.min(this.scrollStartIndex + 1, this.posMap.min);
-      i <= Math.max(this.scrollEndIndex, this.posMap.max);
-      i++
-    ) {
-      if (this.posMap[i] !== undefined && i < this.scrollStartIndex) {
-        delete this.posMap[i];
-      } else if (this.posMap[i] !== undefined && i > this.scrollEndIndex) {
-        delete this.posMap[i];
-      }
-    }
-    this.posMap.min = this.scrollStartIndex + 1;
-    this.posMap.max = this.scrollEndIndex;
-    this.updateRows();
-  }
-
-  private incrementScrollIndex(): void {
-    this.scrollStartIndex += this.cellCount;
-    this.scrollEndIndex += this.cellCount;
-  }
-
-  private decrementScrollIndex(): void {
-    this.scrollStartIndex -= this.cellCount;
-    this.scrollEndIndex -= this.cellCount;
+    return this.columnIndexToLeftMap[columnIndex];
   }
 
   public ngOnDestroy(): void {
